b6d35ce751ecc901465a511879898495fb761ffa
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-08-10 17:15:05 +0400

    Updates

---
+++ assets/css-multi-columns.html-69ed3e56.js
@@ -0,0 +1,7 @@
+import{_ as n,r as a,o as t,c as e,d as c,e as o}from"./app-7efff420.js";const p={},l=o(`<h1 id="css-multi-columns" tabindex="-1"><a class="header-anchor" href="#css-multi-columns" aria-hidden="true">#</a> CSS Multi Columns</h1><div class="language-css" data-ext="css"><pre class="language-css"><code><span class="token selector">article</span> <span class="token punctuation">{</span>
+	<span class="token property">column-count</span><span class="token punctuation">:</span> 2<span class="token punctuation">;</span>              <span class="token comment">/* Кол-во колонок */</span>
+	<span class="token property">column-width</span><span class="token punctuation">:</span> 200px<span class="token punctuation">;</span>          <span class="token comment">/* Ширина колонки */</span>
+	<span class="token property">column-gap</span><span class="token punctuation">:</span> 20px<span class="token punctuation">;</span>             <span class="token comment">/* Gap между колонками */</span>
+	<span class="token property">column-rule</span><span class="token punctuation">:</span> 1px dotted #000<span class="token punctuation">;</span> <span class="token comment">/* Линия между колонками */</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,2);function u(r,i){const s=a("v-iframe");return t(),e("div",null,[l,c(s,{height:"550",src:"https://codepen.io/Sergeenkov/embed/bGGvjwv?height=573&theme-id=0&default-tab=css,result"})])}const d=n(p,[["render",u],["__file","css-multi-columns.html.vue"]]);export{d as default};

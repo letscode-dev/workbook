4c47df41925a08263ddbb3c578a3c75b576d63d3
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-08-17 16:57:42 +0400

    Updates

---
+++ assets/github.html-a485af46.js
@@ -0,0 +1,30 @@
+import{_ as t,r as o,o as c,c as i,a as n,b as e,d as x,f as s,e as l}from"./app-e2fa1f8b.js";const r={},h=n("h1",{id:"хостинг-проектов-github",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#хостинг-проектов-github","aria-hidden":"true"},"#"),e(" Хостинг проектов GitHub")],-1),d={href:"https://www.youtube.com/playlist?list=PL3LQJkGQtzc5rDeb7FjACNb6sOW300yA0",target:"_blank",rel:"noopener noreferrer"},p=n("h3",{id:"работа-с-github",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#работа-с-github","aria-hidden":"true"},"#"),e(" Работа с GitHub")],-1),u={href:"https://github.com/",target:"_blank",rel:"noopener noreferrer"},g=n("li",null,"Создать новый проект на GitHub",-1),m={href:"https://desktop.github.com/",target:"_blank",rel:"noopener noreferrer"},_={href:"https://git-scm.com/downloads",target:"_blank",rel:"noopener noreferrer"},b=n("li",null,"Склонировать репозиторий GitHub через GitHub Desktop",-1),k=n("li",null,"Добавить изменения в локальный репозиторий",-1),f=n("li",null,"Загрузить изменения в удаленный репозиторий",-1),v=n("li",null,[e("Создать новую ветку, добавить изменения и создать Pull Request для merge в ветку "),n("code",null,"main")],-1),G={href:"https://pages.github.com/",target:"_blank",rel:"noopener noreferrer"},H=n("code",null,"README.md",-1),w={href:"https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",target:"_blank",rel:"noopener noreferrer"},N=n("h3",{id:"команды-git",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#команды-git","aria-hidden":"true"},"#"),e(" Команды Git")],-1),E=l(`<h4 id="инициализация" tabindex="-1"><a class="header-anchor" href="#инициализация" aria-hidden="true">#</a> Инициализация</h4><div class="language-bash" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># Инициализация репозитория</span>
+<span class="token function">git</span> init
+</code></pre></div><h4 id="получение-данных" tabindex="-1"><a class="header-anchor" href="#получение-данных" aria-hidden="true">#</a> Получение данных</h4><div class="language-bash" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># Клонирование репозитория</span>
+<span class="token function">git</span> clone https://github.com/AccountName/repo-name.git
+
+<span class="token comment"># Проверить изменения на удаленном репозитории</span>
+<span class="token function">git</span> fetch
+
+<span class="token comment"># Получить изменения с удаленного репозитория</span>
+<span class="token function">git</span> pull
+</code></pre></div><h4 id="отправка-данных" tabindex="-1"><a class="header-anchor" href="#отправка-данных" aria-hidden="true">#</a> Отправка данных</h4><div class="language-bash" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># Индексирование всех измененных файлов</span>
+<span class="token function">git</span> <span class="token function">add</span> <span class="token builtin class-name">.</span>
+
+<span class="token comment"># Сохранение изменений в локальном репозитории</span>
+<span class="token function">git</span> commit <span class="token parameter variable">-m</span> <span class="token string">&#39;text&#39;</span>
+
+<span class="token comment"># Отправка изменений на удаленный репозитория</span>
+<span class="token function">git</span> push origin HEAD
+</code></pre></div><h4 id="работа-с-ветками" tabindex="-1"><a class="header-anchor" href="#работа-с-ветками" aria-hidden="true">#</a> Работа с ветками</h4><div class="language-bash" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># Список веток с указанием текущей</span>
+<span class="token function">git</span> branch
+
+<span class="token comment"># Переход на ветку</span>
+<span class="token function">git</span> checkout НазваниеВетки
+
+<span class="token comment"># Создание ветки</span>
+<span class="token function">git</span> checkout <span class="token parameter variable">-b</span> НазваниеВетки
+
+<span class="token comment"># Слияние текущей и указанной ветки</span>
+<span class="token function">git</span> merge НазваниеВетки
+</code></pre></div>`,8);function V(A,C){const a=o("ExternalLinkIcon");return c(),i("div",null,[h,n("ul",null,[n("li",null,[n("a",d,[e("[playlist] GitHub для новичков"),x(a)])])]),s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),p,s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),n("ol",null,[n("li",null,[e("Зарегистрироваться на "),n("a",u,[e("https://github.com/"),x(a)])]),g,n("li",null,[e("Установить "),n("a",m,[e("GitHub Desktop"),x(a)])]),n("li",null,[e("Установить "),n("a",_,[e("Git"),x(a)]),e(" (необходим для выполненения консольных команд и интеграции с VSCode)")]),b,k,f,v,n("li",null,[e("Подключить "),n("a",G,[e("GitHub Pages"),x(a)]),e(" к проекту на GitHub")]),n("li",null,[e("Скорректировать "),H,e(" с помощью "),n("a",w,[e("Markdown"),x(a)])])]),s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),N,s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),E])}const y=t(r,[["render",V],["__file","github.html.vue"]]);export{y as default};
---
+++ assets/selector-tilda.html-81e19005.js
@@ -0,0 +1,29 @@
+import{_ as p,r as e,o as c,c as o,f as s,d as l,e as u,a as n,b as a}from"./app-e2fa1f8b.js";const i={},x=u(`<h1 id="css-селекторы" tabindex="-1"><a class="header-anchor" href="#css-селекторы" aria-hidden="true">#</a> CSS селекторы (&gt;, +, ~)</h1><div class="language-css" data-ext="css"><pre class="language-css"><code>ul li &gt; span   <span class="token comment">/* Не все span, а только дочерние span */</span>
+ul li + li     <span class="token comment">/* Все последующие элементы li */</span>
+.class1 ~ div  <span class="token comment">/* Все div элементы после class1 */</span>
+.class1 ~ *    <span class="token comment">/* Все элементы после class1 */</span>
+</code></pre></div><div class="language-html" data-ext="html"><pre class="language-html"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>container<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>item<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>item<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>
+		item
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>inside item<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+		<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>inside item<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+	<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>outside<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>outside<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>p</span><span class="token punctuation">&gt;</span></span>outside<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>p</span><span class="token punctuation">&gt;</span></span>
+</code></pre></div><div class="language-css" data-ext="css"><pre class="language-css"><code><span class="token selector">body</span> <span class="token punctuation">{</span>
+	<span class="token property">font-size</span><span class="token punctuation">:</span> 35px<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.container</span> <span class="token punctuation">{</span>
+	<span class="token property">background</span><span class="token punctuation">:</span> silver<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.container &gt; div</span> <span class="token punctuation">{</span>
+	<span class="token property">border</span><span class="token punctuation">:</span> 4px solid blue<span class="token punctuation">;</span>
+	<span class="token property">margin</span><span class="token punctuation">:</span> 10px<span class="token punctuation">;</span>
+	<span class="token property">padding</span><span class="token punctuation">:</span> 10px<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,4),k=n("h3",{id:"примеры",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#примеры","aria-hidden":"true"},"#"),a(" Примеры")],-1),d=n("h4",{id:"custom-checkbox",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#custom-checkbox","aria-hidden":"true"},"#"),a(" Custom Checkbox")],-1);function g(r,m){const t=e("v-iframe");return c(),o("div",null,[x,s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),k,s(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "),s("---------------------------------------------------------"),d,s("---------------------------------------------------------"),l(t,{height:"350",src:"https://codepen.io/Sergeenkov/embed/LMLjYG?height=265&theme-id=default&default-tab=css,result"})])}const v=p(i,[["render",g],["__file","selector-tilda.html.vue"]]);export{v as default};

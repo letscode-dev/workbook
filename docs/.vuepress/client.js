8524818bb8e87da96c2b97d3810116e4ed29e129
Anton Sergeenkov <asergeenkov@combotech.net>
2026-02-21 18:39:29 +0400

    ok

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,9 +1,9 @@
-// TODO: [AppCustom]
 import { defineClientConfig } from "vuepress/client";
 import Layout from "./app-custom/layouts/Layout.vue";
 import NotFound from "@vuepress/theme-default/lib/client/layouts/NotFound.vue";
 
-import "./app-custom/index.scss";
+// TODO: [AppCustom]
+// import "./app-custom/index.scss";
 
 export default defineClientConfig({
   layouts: {
